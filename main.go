fa819079fb1d6dbc70e38ce1ab07302516033537
Gabriel Tarifa <gabrieltarifa@gmail.com>
2025-09-19 21:30:58 -0300

    tunning after 100 iterations w/ local search optimization
    now the psqt/piece value seems working better than previous evaluation. But took
    nearly 10 hours in 3 sessions to get the results. I should try using
    another optimization algorithm like gradient descent or adam to get faster.

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	// "github.com/gabtar/aconcagua/aconcagua"
-	"github.com/gabtar/aconcagua/tuner"
+	"github.com/gabtar/aconcagua/aconcagua"
+	// "github.com/gabtar/aconcagua/tuner"
 )
 
 func main() {
-	// eng := aconcagua.NewEngine()
-	// eng.StartUci()
+	eng := aconcagua.NewEngine()
+	eng.StartUci()
 
-	dataSet := tuner.LoadDataSet("./tuner/training-set/quiet-labeled.epd")
+	// dataSet := tuner.LoadDataSet("./tuner/training-set/quiet-labeled.epd")
 	// params := tuner.GetEvaluationParams()
 	// fmt.Println(params)
 
 	// k := tuner.FindOptimalScalingFactor(dataSet, tuner.GetEvaluationParams())
 	// fmt.Println("Best K", k)
 
-	tuner.Tuner(tuner.ScalingFactor, dataSet, 10)
+	// tuner.Tuner(tuner.ScalingFactor, dataSet, 61)
 }
